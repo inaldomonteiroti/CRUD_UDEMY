1e17d35dd017d4af46ea300058d87c1737c58941
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2020-03-21 17:06:12 -0300

    first commit

---
+++ listar/Conn.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Description of Conn
+ *
+ * @copyright (c) year, Cesar Szpak - Celke
+ */
+class Conn {
+    
+    public static $Host = "localhost";
+    public static $User = "root";
+    public static $Pass = "123mudar";
+    public static $Dbname = "CRUD_UDEMY";
+    private static $Connect = null;
+    
+    private static function Conectar() {
+        try {
+            if(self::$Connect == null):
+                self::$Connect = new PDO('mysql:host=' . self::$Host .';dbname='.self::$Dbname, self::$User, self::$Pass);
+            endif;            
+        } catch (Exception $ex) {
+            echo 'Mensagem: ' . $ex->getMessage();
+            die;
+        }       
+        return self::$Connect;
+    }
+    
+    public function getConn() {
+        return self::Conectar();
+    }
+    
+}
---
+++ listar/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Celke</title>
+    </head>
+    <body>
+        <h1>Listar usuário</h1>
+        <?php
+        require './Conn.php';
+        
+        $nome = "INALDO MARQUES MONTEIRO";
+        $conn = new Conn();
+        $result_user = "SELECT * FROM usuarios WHERE nome=:nome ";
+        
+        $resultado_user = $conn->getConn()->prepare($result_user);
+        $resultado_user->bindParam(':nome', $nome, PDO::PARAM_STR);
+        $resultado_user->execute();
+        
+        while($row_user = $resultado_user->fetch(PDO::FETCH_ASSOC)):
+            echo "ID: " . $row_user['id'] . "<br>";
+            echo "Nome: " . $row_user['nome'] . "<br>";
+            echo "E-mail: " . $row_user['email'] . "<br>";
+            echo "Usuário: " . $row_user['usuario'] . "<br>";
+            echo "Inserido: " . date('d/m/Y H:i:s', strtotime($row_user['created'])) . "<br>";
+            if(!empty($row_user['modified'])):
+                echo "Alterado: " . date('d/m/Y H:i:s', strtotime($row_user['modified'])) . "<br>";
+            endif;
+            echo "<hr>";
+        endwhile;
+        ?>
+    </body>
+</html>
